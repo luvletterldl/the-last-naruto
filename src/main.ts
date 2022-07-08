b67d0d57257fc2de8efc224d7b11f4ed69d56cb2
luvletterldl <2273220457@qq.com>
2022-07-08 12:06:59 +0800

    1 file modified

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,16 @@
 import Vue from 'vue';
 import App from './App.vue';
-import { createRouter, createWebHistory } from 'vue-router';
-import routes from '~pages';
+// import { createRouter, createWebHistory } from 'vue-router';
+// import routes from '~pages';
 import '@unocss/reset/tailwind.css';
 import 'uno.css';
 
 Vue.config.productionTip = false;
 
-const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes,
-});
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes,
+// });
 
 // Vue.use(router);
 new Vue({
