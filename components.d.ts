e73487eaf3bf1571d115b3974fd526054eb74088
ldl <lidongliang01@zuoyebang.com>
2022-12-05 10:53:50 +0800

    chore: update deps

--- components.d.ts
+++ components.d.ts
@@ -2,7 +2,7 @@
 // We suggest you to commit this file into source control
 // Read more: https://github.com/vuejs/core/pull/3399
 export {}
-        
+
 declare module 'vue' {
   export interface GlobalComponents {
     CarbonIcon: typeof import('./src/components/icons/CarbonIcon.vue')['default']
